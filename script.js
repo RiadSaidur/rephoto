3f669ac595b2b0c486d05342bd31540a81f2903d
deathcrush <reidvai1998@gmail.com>
2019-09-04 00:54:44 +0600

    assets update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function scrollAppear(){
     var intro = document.querySelector('.introText');
     var posTop = intro.getBoundingClientRect().top;
     var appear = window.innerHeight/1.5;
-    var hide =window.innerHeight/7;
+    var hide =window.innerHeight/10;
 
     if(posTop < appear){
         intro.classList.add('intro-appear');
