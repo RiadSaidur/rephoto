1cb84b20f055bd4c9e5bfb683cfa10ab46f4b0aa
deathcrush <reidvai1998@gmail.com>
2019-09-05 19:24:13 +0600

    udpates

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-function scrollAppear(){
-    var intro = document.querySelector('.introText');
-    var posTop = intro.getBoundingClientRect().top;
-    var appear = window.innerHeight/1.5;
-    var hide =window.innerHeight/10;
-
-    if(posTop < appear){
-        intro.classList.add('intro-appear');
+if(document.URL.includes("index.html")){
+    function scrollAppear(){
+        var intro = document.querySelector('.introText');
+        var posTop = intro.getBoundingClientRect().top;
+        var appear = window.innerHeight/1.5;
+        var hide =window.innerHeight/10;
+    
+        if(posTop < appear){
+            intro.classList.add('intro-appear');
+        }
+    
+        if(posTop < hide || posTop > appear){
+            intro.classList.remove("intro-appear");
+        }
     }
-
-    if(posTop < hide || posTop > appear){
-        intro.classList.remove("intro-appear");
-    }
-
-    console.log(pos);
-
+    window.addEventListener('scroll', scrollAppear);
 }
 
-window.addEventListener('scroll', scrollAppear);
+if(document.URL.includes("contacts.html")){
+    document.querySelector(".switch").addEventListener('click',function (){
+        document.querySelector(".pivot").classList.toggle("mobile");
+        document.querySelector(".info").classList.toggle("mobile");
+        console.log("clicked");
+    })
+}
